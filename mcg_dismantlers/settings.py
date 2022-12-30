fe80033bc19f0a0b1c07176e9d8b36f8a25a6125
Conor <conor.ganly@yahoo.com>
2022-12-30 18:31:38 +0000

    Create project again to setup in root directory

--- mcg_dismantlers/settings.py
+++ mcg_dismantlers/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-da(ul1&)hz^p(&fz93v=%*s3ef&m5&w92_h4efn1s)=t39b*1('
+SECRET_KEY = 'django-insecure-z8%d7@3&#6j93u$vbt*w47^0_nlq@dbfd*pcz65&ao15c+%s-v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
