6fb852c83ed128675c7e55aefa96c5fbfbea0e5c
Conor <conor.ganly@yahoo.com>
2023-01-02 17:59:02 +0000

    Register checkout models in admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+checkout_models = apps.get_app_config('checkout').get_models()
+
+for model in checkout_models:
+    admin.site.register(model)
