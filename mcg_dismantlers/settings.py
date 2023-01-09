ecc741c4fcfb61ce6aab46575bf1ddffe798987a
Conor <conor.ganly@yahoo.com>
2023-01-09 21:02:39 +0000

    Configure templates diectory in settings.py

--- mcg_dismantlers/settings.py
+++ mcg_dismantlers/settings.py
@@ -73,7 +73,10 @@ ROOT_URLCONF = 'mcg_dismantlers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
