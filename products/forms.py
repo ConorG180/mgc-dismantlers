a1b4d19b2e1e2f8703caa3fd5a7299e903a90ef7
Conor <conor.ganly@yahoo.com>
2023-01-19 02:00:50 +0000

    Create product form to add and edit products

---
+++ products/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Product, Category, Make, Model, Part
+
+
+class Product_form(forms.ModelForm):
+    list_of_makes = Make.objects.all()
+    make = forms.ModelChoiceField(queryset=list_of_makes, to_field_name="name")
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    forms.forms.BaseForm.field_order = ['make', 'car_model']
+
+    # THIS ALLOWS  MODELS TO COME UP BLANK, BUT CURRENTLY CAUSING ERRORS WHEN SUBMITTING.
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['car_model'].queryset = Model.objects.none()
