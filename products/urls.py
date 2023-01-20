ad0e6ce2ad4858c93f98a989171d87c954ad8943
Conor <conor.ganly@yahoo.com>
2023-01-20 16:56:13 +0000

    Create url and view to handle searchs for products

--- products/urls.py
+++ products/urls.py
@@ -18,6 +18,11 @@ urlpatterns = [
         views.add_product,
         name="add_product"
     ),
+    path(
+        "search-product/",
+        views.search_product,
+        name="search_product"
+    ),
     # A
     # Ajax path to load car_models
     path('ajax/load-models/', views.load_models, name='ajax_load_car_models'),  # AJAX
--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import Product, Category, Make, Model, Part
 from .forms import Product_form
+from django.db.models import Q
 
 
 def render_products(request):
@@ -54,6 +56,24 @@ def add_product(request):
             "product_form": product_form,
         }
         return render(request, "products/add-product.html", context)
+
+
+def search_product(request):
+    products = Product.objects.all()
+    if request.method == "GET":
+        user_search_query = request.GET["user_search_query"]
+        if not user_search_query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('products'))
+        queries = Q(color__icontains=user_search_query) | Q(car_model__car_model__icontains=user_search_query) | Q(car_model__make__name__icontains=user_search_query) | Q(part__name__icontains=user_search_query)
+        products = products.filter(queries)
+        context = {
+            "user_search_query": user_search_query,
+            "products": products,
+            "products_count": products.count()
+        }
+        return render(request, 'products/search.html', context)
+    return render(request, 'products/search.html', {})
 
 
 def load_models(request):
