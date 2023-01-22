40d6732ed35eb6f41aab02f901def6d249ed8ee4
Conor <conor.ganly@yahoo.com>
2023-01-22 19:13:05 +0000

    Add view and urls for adding to cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from cart import views
 urlpatterns = [
     path('', views.render_cart, name='cart'),
+    path('add-to-cart/<product_id>', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 
 # Create your views here.
 def render_cart(request):
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    cart[product_id] = 1
+    request.session["cart"] = cart
+    return redirect(reverse('products'))
