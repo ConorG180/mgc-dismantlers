bb0336b13082f5b31851e16de186abd00b6cf675
Conor <conor.ganly@yahoo.com>
2023-01-23 21:11:31 +0000

    Change django admin layout of order models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from django.apps import apps
+from .models import Order, OrderLineItem
 
-checkout_models = apps.get_app_config('checkout').get_models()
 
-for model in checkout_models:
-    admin.site.register(model)
+class OrderLineItemAdmin(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+
+    inlines = (OrderLineItemAdmin,)
+    readonly_fields = ('order_number', 'order_date',
+                        'delivery_cost', 'order_total',
+                        'grand_total', 'original_bag',
+                        'stripe_pid',)
+
+    list_display = [field.name for field in Order._meta.get_fields()]
+
+
+admin.site.register(Order, OrderAdmin)
