139c55e2098fd4ec54bbc3b71ecac69a05510d3f
Conor <conor.ganly@yahoo.com>
2023-01-23 22:32:45 +0000

    Update checkout view to check if cart is empty

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .forms import OrderForm
+from django.contrib import messages
 
 
 def render_checkout(request):
-    order_form = OrderForm()
-    context = {
-        "order_form": order_form
-    }
-    print(order_form)
-    return render(request, 'checkout/checkout.html', context)
+    cart = request.session.get("cart", {})
+    if cart:
+        order_form = OrderForm()
+        context = {
+            "order_form": order_form
+        }
+        print(order_form)
+        return render(request, 'checkout/checkout.html', context)
+    else:
+        messages.error(request, "Add something to your cart to access the checkout page")
+        return redirect(reverse("products"))
