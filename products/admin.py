8e8faf7e35c2c35a0736048916a8b0dc17307b1c
Conor <conor.ganly@yahoo.com>
2023-01-26 03:14:53 +0000

    Customise make model and product models in django admin

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from django.apps import apps
+from .models import Make, Model, Product, Category, Part
 
-products_models = apps.get_app_config('products').get_models()
+# products_models = apps.get_app_config('products').get_models()
 
-for model in products_models:
-    admin.site.register(model)
+# for model in products_models:
+#     admin.site.register(model)
+
+
+@admin.register(Make)
+class MakeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_filter = ("name",)
+
+
+@admin.register(Model)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = [
+        "car_model", "make",
+        "model_year", "vehicle_category"
+    ]
+    list_filter = ("make",)
+    search_fields = ["car_model"]
+
+@admin.register(Product)
+class MakeAdmin(admin.ModelAdmin):
+    list_display = ["car_model", "part", "price", "is_sold", "in_a_cart"]
