b1719250efea2530941921ebc20f459b3f4784c2
Conor <conor.ganly@yahoo.com>
2023-01-26 04:29:29 +0000

    Add makes models parts and years context processors

--- products/context_processors.py
+++ products/context_processors.py
@@ -1,6 +1,16 @@
-from products.models import Category
+from products.models import Category, Make, Model, Part
 
 
 def category_context_processor(request):
     categories = Category.objects.all()
-    return {'categories': categories}
+    makes = Make.objects.all()
+    car_models = Model.objects.all()
+    parts = Part.objects.all()
+    years = range(1950, 2024)
+    return {
+        'categories': categories,
+        'makes': makes,
+        'car_models': car_models,
+        'parts': parts,
+        'years': years,
+    }
