3183db55aaac9487d6e0642df5033309281a8f56
Conor <conor.ganly@yahoo.com>
2023-01-26 04:31:00 +0000

    Create model for wishlist app

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Can't be set up as foreign keys, as these
+# cannot be null and therefore wont' allow 
+# the user to add blank values in the wishlist. 
+# Therefore, just using integer fields to 
+# store each make/model/part id, not the 
+# instances of the models themselves, which 
+# is what the foreign key would do.
+
+
+class Wishlist(models.Model):
+    user = models.ManyToManyField(User)
+    make_id = models.PositiveIntegerField(null=True, blank=True)
+    car_model_id = models.PositiveIntegerField(null=True, blank=True)
+    part_id = models.PositiveIntegerField(null=True, blank=True)
+    model_year = models.PositiveIntegerField(null=True, blank=True)
+    on_add = models.BooleanField()
+    on_sale = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.make} {self.car_model} {self.model_year} {self.part}"
