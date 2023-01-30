516d0c108864bedfb9d1a31219f1fceec922f7f7
Conor <conor.ganly@yahoo.com>
2023-01-30 22:42:28 +0000

    Add automated emails for added parts on wishlist

--- products/views.py
+++ products/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
 from .models import Product, Category, Make, Model, Part
+from wishlist.models import Wishlist
 from .forms import Product_form
 from django.db.models import Q
 from .filters import ProductFilter
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.auth.models import User
 
 
 def render_products(request):
     products = Product.objects.all()
     # print(f"FIRST RENDERING {products}")
     if request.method == "GET":
-        print(f"Here is the get request {request.GET}")
         product_filter = ProductFilter(request.GET, queryset=products)
         # print(f"Here is the filter{dir(product_filter)}")
         products = product_filter.qs
@@ -25,7 +29,6 @@ def render_products(request):
 
 def delete_product(request, product_id):
     product = Product.objects.get(pk=product_id)
-    print(product)
     product.delete()
     return redirect(reverse('products'))
 
@@ -50,19 +53,46 @@ def edit_product(request, product_id):
 def add_product(request):
     product_form = Product_form(request.POST or None, request.FILES)
     if request.method == "POST":
-        print("model ======= ", request.POST.get("car_model"))
         if product_form.is_valid():
-            print("We'gre getting here 44")
+            product = product_form.instance
             product_form.save()
-            print("We'gre getting here 46")
+            # Send email to users who have item added to wishlist
+
+            # Get items on wishlist which only wish to be contacted
+            # When item is added, relevant make/model, and year == 
+            # to chosen year or 0 (Which means all years) and part ==
+            # to chosen year or 0 (Which means all parts).
+            wishlist_entries = Wishlist.objects.filter(Q(on_add=True) & Q(make_id=product.make_id) & Q(car_model_id=product.car_model_id) & (Q(model_year=product.model_year_id) | Q(model_year=0)) & (Q(part_id=product.part_id) | Q(part_id=0)))
+            # Only do something if wishlist isn't empty
+            if wishlist_entries.count():
+                # Iterate over every wishlist entry
+                for wishlist_entry in wishlist_entries:
+                    # put all user emails into emails list
+                    emails = [user.email for user in wishlist_entry.user.all()]
+
+                # Create email subject
+                subject = render_to_string(
+                    'products/wishlist-emails/wishlist_item_added_subject.txt',
+                    {'product': product}
+                )
+                # Create email body
+                body = render_to_string(
+                    'products/wishlist-emails/wishlist_item_added_body.txt',
+                    {'product': product}
+                )
+                # Send emails to emails on wishlist.
+                send_mail(
+                    subject,
+                    body,
+                    settings.DEFAULT_FROM_EMAIL,
+                    emails
+                )
+                print("EMAIL(S) SHOULD BE SENT")
+            print("Wishlist empty, no emails sent")
             return redirect(reverse('products'))
         else:
             print(product_form.errors)
             print("form not valid!!!!!!!!")
-            # print(product_form)
-            # return redirect(reverse('add_product'))
-    # else:
-    # product_form = Product_form()
     context = {
         "product_form": product_form,
     }
@@ -89,11 +119,6 @@ def search_product(request):
 
 def categorize_products(request, cat):
     categorized_products = Product.objects.filter(part__category__friendly_name=cat)
-    # category = Category.objects.get(friendly_name=)
-    # print("variable")
-    print(categorized_products)
-    # print("category object")
-    # print(category)
     context = {
         "category": cat,
         "products": categorized_products
@@ -104,6 +129,5 @@ def categorize_products(request, cat):
 def load_models(request):
     # make = request.GET.get('make')
     make = get_object_or_404(Make, name=request.GET.get("make"))
-    print(make)
     models = Model.objects.filter(make=make).order_by("car_model")
     return render(request, 'products/model_dropdown_list_options.html', {'models': models})
