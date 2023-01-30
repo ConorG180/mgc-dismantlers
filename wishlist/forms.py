ecad763fc38d124c6d3f373aa94c27618277f9ff
Conor <conor.ganly@yahoo.com>
2023-01-30 04:17:30 +0000

    Allow selection of all years and parts in wishlist

--- wishlist/forms.py
+++ wishlist/forms.py
@@ -1,22 +1,31 @@
 from django import forms
 from products.models import Make, Model, Part, Year
 
+# Makes
 makes = Make.objects.all()
-make_choices = tuple([(make.id, make.name) for make in makes])
+make_choices = [(make.id, make.name) for make in makes]
+# Car Models 
 models = Model.objects.all()
-model_choices = tuple([(model.id, model.car_model) for model in models])
+model_choices = [(model.id, model.car_model) for model in models]
+# Parts
 parts = Part.objects.all()
-part_choices = tuple([(part.id, part.name) for part in parts])
+empty_part_choice = (0, "All parts")
+part_choices = [(part.id, part.name) for part in parts]
+part_choices.insert(0, empty_part_choice)
+# Years
 years = Year.objects.all()
-year_choices = tuple([(year.id, year.year) for year in years])
+empty_year_choice = (0, "All years")
+year_choices = [(year.id, year.year) for year in years]
+year_choices.insert(0, empty_year_choice)
+
 
 class WishlistForm(forms.Form):
     make = forms.ChoiceField(choices=make_choices)
     model = forms.ChoiceField(choices=model_choices)
     part = forms.ChoiceField(choices=part_choices)
     year = forms.ChoiceField(choices=year_choices)
-    on_add = forms.BooleanField(label="When part is added", required=False,initial=False)
-    on_sale = forms.BooleanField(label="When part is on sale", required=False,initial=False)
+    on_add = forms.BooleanField(label="When added", required=False,initial=False)
+    on_sale = forms.BooleanField(label="When on sale", required=False,initial=False)
 
     make.widget.attrs.update({'id': 'wishlist_id_make', 'class': 'form-control'})
     model.widget.attrs.update({'id': 'wishlist_id_car_model', 'class': 'form-control'})
