dd5bb231253b5bc5f897a5b7e391d155ed59a5d2
Conor <conor.ganly@yahoo.com>
2023-01-30 04:18:54 +0000

    Change wishlist part and year fields to integer type

--- wishlist/models.py
+++ wishlist/models.py
@@ -14,10 +14,10 @@ class Wishlist(models.Model):
     user = models.ManyToManyField(User)
     make_id = models.PositiveIntegerField(null=True, blank=True)
     car_model_id = models.PositiveIntegerField(null=True, blank=True)
-    part_id = models.PositiveIntegerField(null=True, blank=True)
-    model_year = models.PositiveIntegerField(null=True, blank=True)
+    part_id = models.IntegerField(null=True, blank=True)
+    model_year = models.IntegerField(null=True, blank=True)
     on_add = models.BooleanField()
     on_sale = models.BooleanField()
 
     def __str__(self):
-        return f"{self.make} {self.car_model} {self.model_year} {self.part}"
+        return f"{self.make_id} {self.car_model_id} {self.model_year} {self.part_id}"
