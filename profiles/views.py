ac4da36b3c31370845cfe07e0d7a6208cf73b06f
Conor <conor.ganly@yahoo.com>
2023-02-01 01:27:26 +0000

    Add views and urls for profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.models import User
+from .models import UserProfile
+from checkout.models import Order
+from .forms import UserProfileForm
 
-# Create your views here.
+
+def profile(request):
+  
+    return render(request, 'profiles/profile.html')
