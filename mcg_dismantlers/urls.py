76e75924e571ee83de67346f7744be4882a5b750
Conor <conor.ganly@yahoo.com>
2023-02-02 22:37:41 +0000

    Create 404.html page for broken urls

--- mcg_dismantlers/urls.py
+++ mcg_dismantlers/urls.py
@@ -1,18 +1,4 @@
-"""mcg_dismantlers URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urlsv import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -28,3 +14,5 @@ urlpatterns = [
     path('wishlist/', include('wishlist.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'mcg_dismantlers.views.handler404'
