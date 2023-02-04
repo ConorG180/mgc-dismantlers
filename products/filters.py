a36e2844e1e6b0c3d3fb928e7e81039d4d6c0157
Conor <conor.ganly@yahoo.com>
2023-02-04 22:07:25 +0000

    Add year to product filter

--- products/filters.py
+++ products/filters.py
@@ -7,4 +7,12 @@ class ProductFilter(django_filters.FilterSet):
 
     class Meta:
         model = Product
-        fields = ["make", "car_model", "part", "color", "grade", "fuel"]
+        fields = [
+            "make",
+            "car_model",
+            "model_year",
+            "part",
+            "color",
+            "grade",
+            "fuel"
+        ]
